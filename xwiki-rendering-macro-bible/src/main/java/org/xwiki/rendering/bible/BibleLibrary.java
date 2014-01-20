461b6ba2bde9274fef2ce750e2f0b443f085d976
Matthias Wegner <mic.mat.wegner@web.de>
2014-01-20 21:01:21 +0100

    Fixed Bug to install books on a linux system

--- xwiki-rendering-macro-bible/src/main/java/org/xwiki/rendering/bible/BibleLibrary.java
+++ xwiki-rendering-macro-bible/src/main/java/org/xwiki/rendering/bible/BibleLibrary.java
@@ -15,125 +15,168 @@ import org.crosswire.jsword.book.OSISUtil;
 import org.crosswire.jsword.book.install.InstallException;
 import org.crosswire.jsword.book.install.InstallManager;
 import org.crosswire.jsword.book.install.Installer;
+import org.crosswire.jsword.book.sword.ConfigEntryTable;
 import org.crosswire.jsword.passage.Key;
 import org.crosswire.jsword.passage.NoSuchKeyException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class BibleLibrary {
+public class BibleLibrary
+{
 
-	private static HashMap<String, String> initialsJSWORD2APP = new HashMap<String, String>();
+    private static HashMap<String, String> initialsJSWORD2APP = new HashMap<String, String>();
 
-	private static HashMap<String, String> initialsAPP2JSWORD = new HashMap<String, String>();
+    private static HashMap<String, String> initialsAPP2JSWORD = new HashMap<String, String>();
 
-	public static void setInitialMap(String jswordInitials, String appInitials) {
-		initialsJSWORD2APP.put(jswordInitials, appInitials);
-		initialsAPP2JSWORD.put(appInitials, jswordInitials);
-	}
+    public static void setInitialMap(String jswordInitials, String appInitials)
+    {
+        initialsJSWORD2APP.put(jswordInitials, appInitials);
+        initialsAPP2JSWORD.put(appInitials, jswordInitials);
+    }
 
-	static {
-		setInitialMap("ASV", "ASV");
-		setInitialMap("Jubilee2000", "JUB");
-		setInitialMap("ESV", "ESV");
-		setInitialMap("GerElb1905", "ELB");
-		setInitialMap("GerSch", "SCH");
-		setInitialMap("GerHfa2002", "HFA");
-		setInitialMap("GerLut1912", "LUT");
-		setInitialMap("GerNeUe", "NEU");
-	}
+    static {
+        // java.util.logging.Logger log =
+        // java.util.logging.LogManager.getLogManager().getLogger(" org.crosswire.jsword.book.sword.ConfigEntryTable");
+        // for (java.util.logging.Handler h : log.getHandlers()) {
+        // h.setLevel(java.util.logging.Level.OFF);
+        // }
+        try {
+//            org.apache.log4j.Logger.getLogger("org.springframework").setLevel(org.apache.log4j.Level.OFF);
+            java.util.logging.Logger.getLogger("org.crosswire").setLevel(
+                java.util.logging.Level.OFF);
+//            Logger root = (Logger)LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+//            root.setLevel(org.slf4j.Level.DEBUG); 
+        } catch (Exception e) {
+        }
 
-	public static List<Book> getBooks() {
-		return Books.installed().getBooks(BookFilters.getBibles());
-	}
+        setInitialMap("ASV", "ASV");
+        setInitialMap("Jubilee2000", "JUB");
+        setInitialMap("ESV", "ESV");
+        setInitialMap("GerElb1905", "ELB");
+        setInitialMap("GerSch", "SCH");
+        setInitialMap("GerHfa2002", "HFA");
+        setInitialMap("GerLut1912", "LUT");
+        setInitialMap("GerNeUe", "NEU");
+    }
 
-	public static String getCanonicalText(Book book, String name) {
-		try {
-			Key key = book.getKey(name);
-			if (book.contains(key)) {
-				BookData data = new BookData(book, key);
-				String text = OSISUtil.getCanonicalText(data.getOsisFragment());
-				return text;
-			}
+    public static List<Book> getBooks()
+    {
+        return Books.installed().getBooks(BookFilters.getBibles());
+    }
 
-		} catch (NoSuchKeyException e) {
-			// error = true;
-			return e.getMessage();
-		} catch (BookException e) {
-			// error = true;
-			return e.getMessage();
-		} catch (StringIndexOutOfBoundsException e) {
-			// error = true;
-			return "Not Found / Nicht gefunden";
-		}
-		return null;
-	}
+    public static String getCanonicalText(Book book, String name)
+    {
+        try {
+            Key key = book.getKey(name);
+            if (book.contains(key)) {
+                BookData data = new BookData(book, key);
+                String text = OSISUtil.getCanonicalText(data.getOsisFragment());
+                return text;
+            }
 
-	public static String getCanonicalText(String bibleAndVerse) {
-		// boolean error = false;
+        } catch (NoSuchKeyException e) {
+            // error = true;
+            return e.getMessage();
+        } catch (BookException e) {
+            // error = true;
+            return e.getMessage();
+        } catch (StringIndexOutOfBoundsException e) {
+            // error = true;
+            return "Not Found / Nicht gefunden";
+        }
+        return null;
+    }
 
-		if (bibleAndVerse != null) {
+    public static String getCanonicalText(String bibleAndVerse)
+    {
+        // boolean error = false;
 
-			if (!bibleAndVerse.contains("#")) {
-				return "Symbol # between Bible and Verse is missing";
-			}
-			
-			int index = bibleAndVerse.indexOf("#");
-			String initials = bibleAndVerse.substring(0, index);
-			String name = bibleAndVerse.substring(index + 1, bibleAndVerse.length());
+        if (bibleAndVerse != null) {
 
-			if (containsInitials(initials)) {
-				if (getBook(initials) != null) {
-					return getCanonicalText(getBook(initials), name);
-				} else {
-					// error = true;
-					return "book is not installed";
-				}
-			} else {
-				// error = true;
-				return "book with initials is not known, must be ASV, JUB, ESV, ELB, SCH, HFA, LUT, NEU";
-			}
+            if (!bibleAndVerse.contains("#")) {
+                return "Symbol # between Bible and Verse is missing";
+            }
 
-		} else {
-			return "no key given";
-		}
+            int index = bibleAndVerse.indexOf("#");
+            String initials = bibleAndVerse.substring(0, index);
+            String name = bibleAndVerse.substring(index + 1, bibleAndVerse.length());
 
-	}
+            if (containsInitials(initials)) {
+                if (getBook(initials) != null) {
+                    return getCanonicalText(getBook(initials), name);
+                } else {
+                    // error = true;
+                    return "book is not installed";
+                }
+            } else {
+                // error = true;
+                return "book with initials is not known, must be ASV, JUB, ESV, ELB, SCH, HFA, LUT, NEU";
+            }
 
-	public static Book getBook(String bookInitials) {
-		return Books.installed().getBook(initialsAPP2JSWORD.get(bookInitials));
-	}
+        } else {
+            return "no key given";
+        }
 
-	private static boolean containsInitials(String initials) {
-		return initialsAPP2JSWORD.containsKey(initials);
-	}
+    }
 
-	public static void initialize() {
-		InstallManager imanager = new InstallManager();
+    public static Book getBook(String bookInitials)
+    {
+        return Books.installed().getBook(initialsAPP2JSWORD.get(bookInitials));
+    }
 
-		Map installers = imanager.getInstallers();
-		Iterator iter = installers.entrySet().iterator();
-		String name = null;
-		while (iter.hasNext()) {
-			Map.Entry mapEntry = (Map.Entry) iter.next();
-			name = (String) mapEntry.getKey();
-			Installer installer = (Installer) mapEntry.getValue();
-			try {
-				installer.reloadBookList();
-//				installer.getBooks();
-			} catch (InstallException e) {
-				e.printStackTrace();
-			}
-		}
+    private static boolean containsInitials(String initials)
+    {
+        return initialsAPP2JSWORD.containsKey(initials);
+    }
 
-	}
+    public static void initialize()
+    {
+        InstallManager imanager = new InstallManager();
 
-	private static class SimpleBookFilter implements BookFilter {
-		public SimpleBookFilter(String bookKey) {
-			name = bookKey;
-		}
+        Map installers = imanager.getInstallers();
+        Iterator iter = installers.entrySet().iterator();
+        String name = null;
+        while (iter.hasNext()) {
+            Map.Entry mapEntry = (Map.Entry) iter.next();
+            name = (String) mapEntry.getKey();
+            Installer installer = (Installer) mapEntry.getValue();
+            System.out.println();
+            try {
+                installer.reloadBookList();
+                // installer.getBooks();
+                // installer.install(arg0);
+                for (Object bookObject : installer.getBooks()) {
+                    Book book = (Book) bookObject;
 
-		public boolean test(Book bk) {
-			return bk.getInitials().equals(name);
-		}
+                    if (initialsJSWORD2APP.containsKey(book.getInitials())) {
+                        if (Books.installed().getBook(book.getInitials()) == null) {
+                            installer.install(book);
+                            System.out.println("Install book '" + book.getInitials() + "'");
+                        }
+                    }
+                }
 
-		private String name;
-	}
+            } catch (InstallException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Books.installed().addBook(book);
+
+    }
+
+    private static class SimpleBookFilter implements BookFilter
+    {
+        public SimpleBookFilter(String bookKey)
+        {
+            name = bookKey;
+        }
+
+        public boolean test(Book bk)
+        {
+            return bk.getInitials().equals(name);
+        }
+
+        private String name;
+    }
 }
